1dfc77f8b9ee26bad349705c5ca93c4388d49246
nidhinbabujessy <118510554+nidhinbabujessy@users.noreply.github.com>
2023-07-12 12:06:55 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        delegate int referance(int n);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(8));
+            Console.WriteLine(mult(8));
+
+            referance info = new referance(sum);
+            Console.WriteLine(info(4));
+
+            referance inf2 = new referance(mult);
+            Console.WriteLine(inf2(4));
+        }
+
+        static int sum(int a)
+        {
+            return a + 10;
+        }
+
+        static int mult(int a)
+        {
+            return a * 10;
+        }
+    }
+}
